13d267b16d4a1ba303a4aba39dce8c86a885e688
Katalin Csortos <katalin.csortos@gmail.com>
2018-09-25 11:21:31 +0200

    Removed whitespaces

--- sudoku.py
+++ sudoku.py
@@ -5,54 +5,56 @@ import csv
 from termcolor import colored
 import time
 
-sudoku_title =r"""
-    
-     ___         _       _        
-    / __> _ _  _| | ___ | |__ _ _ 
+sudoku_title = r"""
+
+     ___         _       _
+    / __> _ _  _| | ___ | |__ _ _
     \__ \| | |/ . |/ . \| / /| | |
     <___/`___|\___|\___/|_\_\`___|
-                              
+
 
  """
 
 
 winner_giraffe = r"""
- 
+
                                        ._ o o
                                        \_`-)|_
-                                    ,""       \ 
-                                  ,"  ## |   ಠ ಠ. 
-                                ," ##   ,-\__    `.    
-                              ,"       /     `--._;)   You won! 
-                            ,"     ## /                
+                                    ,""       \
+                                  ,"  ## |   ಠ ಠ.
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)   You won!
+                            ,"     ## /
                           ,"   ##    /
- 
- 
+
+
                     """
 
 
 loser_giraffe = r"""
- 
+
                                        ._ o o
                                        \_`-)|_
-                                    ,""       \ 
-                                  ,"  ## |   ಠ ಠ. 
-                                ," ##   ,-\__    `.    
-                              ,"       /     `--._;)   Your solution is not correct. You lost!! 
-                            ,"     ## /                
+                                    ,""       \
+                                  ,"  ## |   ಠ ಠ.
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)   Your solution is not correct. You lost!!
+                            ,"     ## /
                           ,"   ##    /
- 
- 
+
+
                     """
 
 
-valid_numbers = [1,2,3,4,5,6,7,8,9]
+valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-#This prints the ASCII art
+
+# This prints the ASCII art
 def print_title(ascii):
     print(ascii)
 
-#Loading the gameplay csv template and initializing that grid it into a new local list
+
+# Loading the gameplay csv template and initializing that grid it into a new local list
 def load_csv(filename):
     with open("{}.csv".format(filename), "r") as f:
         reader = list(csv.reader(f))
@@ -61,14 +63,16 @@ def load_csv(filename):
                 lst[i] = int(lst[i])
         return reader
 
-#This function is used to overwrite the original grid locally
+
+# This function is used to overwrite the original grid locally
 def update_csv(filename, matrix):
     with open("{}.csv".format(filename), "w") as f:
         writer = csv.writer(f)
         for row in matrix:
             writer.writerow(row)
 
-#Printing the local grid and coloring it. White = new numbers which were 0 in the original file
+
+# Printing the local grid and coloring it. White = new numbers which were 0 in the original file
 def print_sudoku(grid, level):
     board = load_csv("{}_puzzle".format(grid))
     orig_board = load_csv("{}_puzzle".format(level))
@@ -91,7 +95,8 @@ def print_sudoku(grid, level):
         else:
             print("+" + "   +"*9)
 
-#Asking the player to add the row number
+
+# Asking the player to add the row number
 def user_row():
     row = input("Choose a row: ")
     if row.isdigit():
@@ -102,10 +107,11 @@ def user_row():
             print("That's not a valid number!")
             return user_row()
     else:
-       print("That's not a number!")
-       return user_row()
+        print("That's not a number!")
+        return user_row()
 
-#Asking the player to add the column number
+
+# Asking the player to add the column number
 def user_column():
     column = input("Choose a column: ")
     if column.isdigit():
@@ -116,10 +122,11 @@ def user_column():
             print("That's not a valid number!")
             return user_column()
     else:
-       print("That's not a number!")
-       return user_column()
+        print("That's not a number!")
+        return user_column()
 
-#Asking the player to add the number
+
+# Asking the player to add the number
 def user_number():
     number = input("Add a number from 1 to 9: ")
     if number.isdigit():
@@ -130,17 +137,19 @@ def user_number():
             print("That's not a valid number!")
             return user_number()
     else:
-       print("That's not a number!")
-       return user_number()
+        print("That's not a number!")
+        return user_number()
 
-#Comparing the local gameplay grid to the original one
+
+# Comparing the local gameplay grid to the original one
 def check_in_orig_puzzle(row, column, grid):
     if int(load_csv(grid)[row][column]) != 0:
         return True
     else:
         return False
 
-#Checking if the gampley grid still includes 0
+
+# Checking if the gampley grid still includes 0
 def check_for_zero(grid):
     gameplay = load_csv("{}_puzzle".format(grid))
     for i in range(0, len(gameplay)):
@@ -149,7 +158,8 @@ def check_for_zero(grid):
                 return True
     return False
 
-#Resetting the game
+
+# Resetting the game
 def new_game():
     new = input("Would you like to play again? (y / n) ").lower()
     if new == "y":
@@ -160,15 +170,18 @@ def new_game():
         print("Invalid input!")
         return new_game()
 
-#After the grid is filled, user can still edit it, then check if he/she won and exit or restart the game
+
+# After the grid is filled, user can still edit it, then check if he/she won and exit or restart the game
 def check_continue(grid, level):
     user_answer = input("Would you like to change anything in your solution? (y / n) ")
     gameplay = load_csv("{}_puzzle".format(grid))
     easy_solution = load_csv("{}_solution".format(level))
     if user_answer == "n".lower():
         if gameplay == easy_solution:
+            sleep(1)
             print_title(winner_giraffe)
         else:
+            sleep(1)
             print_title(loser_giraffe)
         return False
     elif user_answer == "y".lower():
@@ -177,14 +190,15 @@ def check_continue(grid, level):
         print("Invalid input. Please try again.")
         return check_continue(grid, level)
 
-#Adding a new number to the grid, also checking if the index was 0 in the original layout
+
+# Adding a new number to the grid, also checking if the index was 0 in the original layout
 def add_number(user_row, user_column, user_number, grid, level):
-    u_row = user_row() - 1     #u = user
-    u_column = user_column() - 1    #The 1st element is the 0th, that's why -1
+    u_row = user_row() - 1  # u = user
+    u_column = user_column() - 1    # The 1st element is the 0th, that's why -1
     if check_in_orig_puzzle(u_row, u_column, "{}_puzzle".format(level)):
         print("You can't overwrite the original layout!")
         return "continue"
-    else: 
+    else:
         add_num = user_number()
         gameplay = load_csv("{}_puzzle".format(grid))
         gameplay[u_row][u_column] = add_num
@@ -199,7 +213,8 @@ def add_number(user_row, user_column, user_number, grid, level):
         else:
             return True
 
-#User can choose diffculty
+
+# User can choose diffculty
 def select_level():
     level = input("\tChoose your level: ")
     if level not in ["1", "2", "3"]:
@@ -223,13 +238,15 @@ def print_levels():
     elif level == "3":
         return "notfun"
 
-#Starting the game with a clear terminel to avoid "jumping line"
+
+# Starting the game with a clear terminel to avoid "jumping line"
 def start_screen(orig_grid):
     os.system('clear')
     print_sudoku(orig_grid, orig_grid)
     return orig_grid
 
-#The actual game:
+
+# The actual game:
 def start_game(user_row, user_column, user_number, gameplay, level):
     start = time.time()
     while True:
@@ -246,5 +263,6 @@ def start_game(user_row, user_column, user_number, gameplay, level):
     if new_game():
         start_game(user_row, user_column, user_number, "gameplay", start_screen(print_levels()))
 
-#Let the fun begin
+
+# Let the fun begin
 start_game(user_row, user_column, user_number, "gameplay", start_screen(print_levels()))
